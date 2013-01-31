c281ec586e4d8a334737becd7c67f2a4201281fa
Pranav M. Gandhi <hyfi@missilecommand.ccs.neu.edu>
2013-01-30 21:10:21 -0500

    Established flow for the program

--- Project2/webcrawler.py
+++ Project2/webcrawler.py
@@ -2,6 +2,7 @@
 
 import sys
 import socket
+import time
 
 username = sys.argv[1]
 password = sys.argv[2]
@@ -10,6 +11,9 @@ port = 80;
 
 visited_links = []
 secret_flags = []
+link_queue = ["/fakebook"] # Start with /facebook
+csrf_cookie = ""
+sessionid = ""
 
 # Create socket connection and return a socket
 def connect():
@@ -20,10 +24,30 @@ def connect():
 def have_we_been_there_yet(link):
   return link in visited_links
 
+def launch_thread():
+  link = link_queue.pop()
+  html = get_page(link) # Handles error messages
+  try_to_find_flags() # Will add to secret_flags
+  get_new_links() # Will add to link_queue
 
 
+
+
+
+
+
+
+# Connect to server and login
 sock = connect()
+do_login()
 
-
+# Keep launching threads until we have 5 flags
 while len(secret_flags) < 5:
+  if len(link_queue) > 0:
+    launch_thread()
+  else:
+    time.sleep(1) # Give it a chance to find more links
 
+# Print flags at the end
+for flag in secret_flags:
+  print flag
