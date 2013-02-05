307942eb6e225e6e9ad7c3680cad074e8df5c650
chiner <chiner@ccs.neu.edu>
2013-02-04 21:41:31 -0500

    maybe broke some things. added headers to the do-login part. fixed cookies in the login request. tried turning off threading (didn't work...)

--- Project2/webcrawler.py
+++ Project2/webcrawler.py
@@ -6,6 +6,7 @@ import time
 import threading
 
 from HTMLParser import HTMLParser
+from urlparse import urlparse
 
 username = sys.argv[1]
 password = sys.argv[2]
@@ -23,9 +24,12 @@ cookies = [] # An array of tuples. a tuple is a (name_of_cookie, value_of_cookie
 class LinkParser( HTMLParser ):
     def handle_starttag( self, tag, attrs ):
         if tag == 'a':
-            link = next( sub1 for sub1 in attrs if 'href' in sub1 )[1]
+            o = urlparse( next( sub1 for sub1 in attrs if 'href' in sub1 )[1] )
 
-            # print link
+            # get just the directory...
+            link = o.path
+
+            print link
             if not have_we_been_there_yet( link ):
                 link_queue.append( link )
 
@@ -67,11 +71,14 @@ class Launch_Thread(threading.Thread):
 
 # Handle all login related things
 def do_login():
-  headers = get_headers("/accounts/login/") # Send get request to get cookies
+  print "login"
+  headers = get_headers("/accounts/login/?next=/fakebook/") # Send get request to get cookies
   set_cookies(headers) # Save those 2 cookies
   headers = generate_login_headers() # Create a new POST request to login with the cookies
   headers = send(headers) # Send the POST request to the server
+  print headers
   set_cookies(headers) # Save any updated cookies. We should be logged in now.
+  print "login complete"
 
 # Get the requested page and send all the cookies while doing it
 def get_page(link):
@@ -105,7 +112,7 @@ Host: cs5700f12.ccs.neu.edU
 
 # Just send a message to the server and return the reply
 def send(raw):
-  sock = connect()
+  # sock = connect()
   sock.send(raw)
   return sock.recv(10000)
 
@@ -122,27 +129,56 @@ def set_cookies(headers):
 
 def generate_login_headers():
   cookie_s = ""
+  cookie_h = ""
   for cookie in cookies:
     cookie_s = cookie_s + cookie[0] + "=" + cookie[1]
+    cookie_s = cookie_s + ";"
+
+    if cookie[0] == "csrftoken":
+        cookie_h = "csrfmiddlewaretoken=" + cookie[1]
+
+
+  data = """username={username}&password={password}&{cookie_h}&next=%2Ffakebook%2F""".format(username=username,password=password,cookie_h=cookie_h)
+  content_length = len( data )
 
   headers = """POST /accounts/login HTTP/1.1
   Host: cs5700f12.ccs.neu.edu
-  Referrer: cs5700f12.ccs.neu.edu
+  Connection: keep-alive
+  Content-Length: {content_length}
+  Cache-Control: max-age=0
+  Accept: text/html,application/xhtml+xml,application/xml
+  Origin: http://cs5700f12.ccs.neu.edu
+  User-Agent: Mozilla/5.0 (X11; Linux i686)
+  Content-Type: application/x-www-form-urlencoded
+  Referer: http://cs5700f12.ccs.neu.edu/accounts/login/?next=/fakebook/
+  Accept-Encoding: identity
+  Accept-Language: en-US,en;q=0.8
+  Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3
+  Cookies:{cookie_s}
+  Content-Encoding: identity
 
-  username={username}&password={password}&{cookie_s}
+  {data}
 
 
-  """
+  """.format(cookie_s=cookie_s,content_length=content_length,data=data)
+  print headers
+
   return headers
 
+sock = connect()
 
 # Connect to server and login
 do_login()
 
+
 # Keep launching threads until we have 5 flags
-while len(secret_flags) < 5:
+while len(secret_flags) < 5 and False:
   if len(link_queue) > 0:
-    Launch_Thread().start()
+    link = link_queue.pop()
+    html = get_page(link) # Handles error messages
+    try_to_find_flags(html) # Will add to secret_flags
+    get_new_links(html) # Will add to link_queue
+    #    Launch_Thread().start()
   else:
     time.sleep(1) # Give it a chance to find more links
 
