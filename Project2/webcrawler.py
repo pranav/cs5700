d5765a3465aff66687ba90d5ae44e9183f6b249e
Pranav M. Gandhi <hyfi@missilecommand.ccs.neu.edu>
2013-02-06 00:08:13 -0500

    Logging in success. Need to re-set session id though

--- Project2/webcrawler.py
+++ Project2/webcrawler.py
@@ -29,7 +29,6 @@ class LinkParser( HTMLParser ):
             # get just the directory...
             link = o.path
 
-            print link
             if not have_we_been_there_yet( link ):
                 link_queue.append( link )
 
@@ -71,15 +70,11 @@ class Launch_Thread(threading.Thread):
 
 # Handle all login related things
 def do_login():
-  print "login"
   headers = get_headers("/accounts/login/?next=/fakebook/") # Send get request to get cookies
   set_cookies(headers) # Save those 2 cookies
   headers = generate_login_headers() # Create a new POST request to login with the cookies
   headers = send(headers) # Send the POST request to the server
-  print headers
-  set_cookies(headers) # Save any updated cookies. We should be logged in now.
-  print cookies
-  print "login complete"
+  #set_cookies(headers) # Save any updated cookies. We should be logged in now.
 
 # Get the requested page and send all the cookies while doing it
 def get_page(link):
@@ -103,7 +98,7 @@ def stringify_cookies():
 def get_headers(link):
   headers = """
 GET {link} HTTP/1.1
-Host: cs5700f12.ccs.neu.edU
+Host: cs5700f12.ccs.neu.edu
 
 
 
@@ -115,7 +110,11 @@ Host: cs5700f12.ccs.neu.edU
 def send(raw):
   sock = connect()
   sock.send(raw)
-  return sock.recv(10000)
+  reply = sock.recv(10000)
+  print raw
+  print reply
+  sock.close()
+  return reply
 
 
 # Uses raw headers, and gets cookies out of them
@@ -127,57 +126,36 @@ def set_cookies(headers):
       cookie_value = line.split()[1].split('=')[1].split(';')[0]
       cookies.append((cookie_name, cookie_value))
 
-
+# Generate the headers that would do the login
 def generate_login_headers():
-  cookie_s = ""
-  cookie_h = ""
-  for cookie in cookies:
-    cookie_s = cookie_s + cookie[0] + "=" + cookie[1]
-    cookie_s = cookie_s + ";"
+  # Cookies are [(cookie_name, cookie_value)]
+  # Generate cookie string
+  csrftoken = dict(cookies)['csrftoken']
+  cookie_str = "Cookie: "+cookies[0][0]+'='+cookies[0][1]+"; "+cookies[1][0]+"="+cookies[1][1]
+  post_data = "csrfmiddlewaretoken="+csrftoken+"&username="+username+"&password="+password+"&next=%2Ffakebook%2F"
+  content_len = len(post_data)
 
-    if cookie[0] == "csrftoken":
-        cookie_h = "csrfmiddlewaretoken=" + cookie[1]
-
-
-  data = """username={username}&password={password}&{cookie_h}&next=%2Ffakebook%2F""".format(username=username,password=password,cookie_h=cookie_h)
-  content_length = len( data )
-
-  headers = """POST /accounts/login HTTP/1.1
+  return """POST /accounts/login/ HTTP/1.1
 Host: cs5700f12.ccs.neu.edu
-Connection: keep-alive
-Content-Length: {content_length}
-Cache-Control: max-age=0
-Accept: text/html,application/xhtml+xml,application/xml
 Origin: http://cs5700f12.ccs.neu.edu
-User-Agent: Mozilla/5.0 (X11; Linux i686)
-Content-Type: application/x-www-form-urlencoded
-Referer: http://cs5700f12.ccs.neu.edu/accounts/login/?next=/fakebook/
-Accept-Encoding: identity
-Accept-Language: en-US,en;q=0.8
-Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3
-Cookies:{cookie_s}
-Content-Encoding: identity
+X-CSRFToken: {csrftoken}
+Content-Length: {content_len}
+{cookie_str}
 
-{data}
+{post_data}
+
+""".format(csrftoken=csrftoken,content_len=content_len,cookie_str=cookie_str,post_data=post_data)
 
 
-""".format(cookie_s=cookie_s,content_length=content_length,data=data)
-  print headers
-
-  return headers
 
 # Connect to server and login
 do_login()
 
 
 # Keep launching threads until we have 5 flags
-while len(secret_flags) < 5 and False:
+while len(secret_flags) < 5:
   if len(link_queue) > 0:
-    link = link_queue.pop()
-    html = get_page(link) # Handles error messages
-    try_to_find_flags(html) # Will add to secret_flags
-    get_new_links(html) # Will add to link_queue
-    #    Launch_Thread().start()
+    Launch_Thread().start()
   else:
     time.sleep(1) # Give it a chance to find more links
 
