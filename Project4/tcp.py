46b496a68c115463f988bb8bda674a894aade2a5
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-03-18 15:20:41 -0400

    Initial version of tcp.py

---
+++ Project4/tcp.py
@@ -0,0 +1,17 @@
+
+"""
+  TCP Variant known as Shoe
+  Wraps over Socket class
+  Redefines connect, send, recv, close
+
+"""
+
+class Shoe:
+  def connect:
+
+  def send:
+
+  def recv:
+
+  def close:
+
