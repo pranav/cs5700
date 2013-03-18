fac7ead8ab28c75e2906a935b8eaf2e634dd9119
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-03-18 15:56:30 -0400

    Implemented IP. Good to go

--- Project4/tcp.py
+++ Project4/tcp.py
@@ -6,12 +6,61 @@
 
 """
 
+import socket, random
+from struct import *
+
 class Shoe:
+
+  def socket:
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, IPPROTO_RAW)
+    
+
+
   def connect:
+    print "CONNECT"
 
   def send:
+    print "SEND"
 
   def recv:
+    print "RECV"
 
   def close:
+    print "CLOSE"
+
+class IP:
+  # Internet header length (howm many 32bit words are the header)
+  self.ihl = 5
+  # Version (IPv4)
+  self.ver = 4
+  # Type of Service
+  self.ip_tos = 0
+  # Size of the entire packet
+  self.ip_total_len = 0
+  # ID of this ip datagram
+  self.ip_id = self.random_ip_id()
+  # Fragment offset (we aren't using this)
+  self.ip_frag_off = 0
+  # IP Time to live
+  self.ip_ttl = 255
+  # This is TCP 
+  self.ip_proto = socket.IPPROTO_TCP
+  # 16bit Checksum
+  self.ip_check = 0
+  # Source IP
+  self.source_ip = socket.inet_aton(socket.gethostbyname(socket.gethostname()))
+  # Destination IP
+  self.ip_destination = 'help me'
+  self.ip_ihl_ver = (version << 4) + self.ihl
+
+  def __init__(self, destination):
+    self.ip_destination = socket.inet_aton(destination)
+
+  def get_header(self):
+    return pack('!BBHHHBBH4s4s', self.ip_ihl_ver, self.ip_tos, self.ip_total_len, self.ip_id, self.ip_frag_off, ip_ttl, self.ip_proto, self.ip_check, self.ip_destination)
+
+  def random_ip_id(self):
+    return random.randint(100,6000)
+
+
 
